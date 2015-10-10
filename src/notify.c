387e8866c5aa938ce15f8450621ba8b033b3288a
Jacob Riddle <ironlenny@gmail.com>
2015-10-10 11:53:44 -0700

    Add notify_addWatch() definition.

---
+++ src/notify.c
@@ -0,0 +1,9 @@
+#include "notify.h"
+
+int notify_addWatch (char const * path, int notify_events) {
+    if ( !inotifytools_initialize () ||
+	 !inotify_watch_file ( path, notify_events ) ) {
+    fprintf(stderr, "%s\n", strerror( inotifytools_error() ) );
+  }
+    return 1;
+}
