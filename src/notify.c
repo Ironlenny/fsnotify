a8622ac8ba0955aaf800d9e4eedd241c96e5b61c
Jacob Riddle <ironlenny@gmail.com>
2015-10-10 12:29:15 -0700

    Fix bug in notify_addWatch().
    inotifytools_watch_file() was incorrectly inotify_wath_file().

--- src/notify.c
+++ src/notify.c
@@ -2,7 +2,7 @@
 
 int notify_addWatch (char const * path, int notify_events) {
     if ( !inotifytools_initialize () ||
-	 !inotify_watch_file ( path, notify_events ) ) {
+	 !inotifytools_watch_file ( path, notify_events ) ) {
     fprintf(stderr, "%s\n", strerror( inotifytools_error() ) );
   }
     return 1;
